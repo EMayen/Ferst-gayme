3b735ed46fce9fa3766fcbaab9eaea69fa27d356
Erick Mayen <emayenperez.de@mysummitps.org>
2016-10-20 18:10:40 +0000

    update

--- MyGame/myGame.js
+++ MyGame/myGame.js
@@ -1,20 +1,27 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
-    preload: preload,
-    create: create,
-    update: update
-});
+/* global Phaser */
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var platforms;
 var player;
 var cursors;
 var stars;
+var scoreText;
+var score;
+var music;
+
 function preload() {
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.pg');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.image('star', 'assets/Drawing.png');
+    game.load.image('dude', 'assets/dude.png');
+    game.load.audio('merio', 'assets/merio.mp3');
 }
 
 function create() {
+    
+    music = game.add.audio('merio');
+    
+    music.play();
+    
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.add.sprite(0, 0, 'star');
     game.add.sprite(0, 0, 'sky');
@@ -25,22 +32,24 @@ function create() {
     ground.body.immovable = true;
     var ledge = platforms.create(0, 400, 'ground');
     ledge.body.immovable = true;
-    player = game.add.sprite(0, game.world.height - 111, 'dude');
+    player = game.add.sprite (500, 50, 'dude');
     game.physics.arcade.enable(player);
-    player.body.bounce.y = 1;
-    player.body.gravity.y = 1;
+    player.body.bounce.y = 0;
+    player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
     cursors = game.input.keyboard.createCursorKeys();
-    stars = game.add.group();
+        stars = game.add.group();
     stars.enableBody = true;
-    for (var i = 0; i < 60; i++)
-    {
+    for (var i = 0; i < 60; i++){
+    
         var star = stars.create(i*70, 0, 'star');
-        star.body.gravity.y = 6;
+        star.body.gravity.y = 200;
         star.body.bounce.y = 0.7 + Math.random() * 0.2;
     }
+    scoreText = game.add.text(16,16, 'score: 0', {fontSize: '32px' , fill: '#000'});
+    score = 0;
+    merio.loopFull(true);
+    
 }
 
 function update() {
@@ -48,16 +57,16 @@ function update() {
     player.body.velocity.x = 0;
 
     if (cursors.left.isDown) {
-        player.body.velocity.x = -150;
-        player.animations.play('left');
+        player.body.velocity.x = -1000;
+
     }
     else if (cursors.right.isDown) {
-        player.body.velocity.x = 150;
-        player.animations.play('right');
+        player.body.velocity.x = 1000;
+
     }
     else {
         player.animations.stop();
-        player.frame = 4;
+        player.frame =0;
     }
     if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
         player.body.velocity.y = -350;
@@ -66,5 +75,13 @@ function update() {
     game.physics.arcade.overlap(player, stars, collectStar, null, this)
 }
 function collectStar (player, star) {
-    star.kill
+    star.kill();
+    score += 1
+    scoreText.text = 'score:' + score;
+     for (var i = 0; i < 2; i++){
+    
+        var star = stars.create(Math.random()*800, 50, 'star');
+        star.body.gravity.y = 200;
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    }
 }
